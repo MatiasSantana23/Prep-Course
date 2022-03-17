07355bbca114624c39ca252c072c30e3b8725c93
MatiasSantana23 <matiassantana02@outlook.com>
2022-03-17 10:58:00 -0300

    im only getting started

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -27,7 +27,8 @@ const nuevoModulo = 21 % 5 === null;
 
 function devolverString(str) {
   // "Return" la string provista: str
-  // Tu código:
+  // Tu código:no cambies los nombres de las funciones
+
   
 }
 
